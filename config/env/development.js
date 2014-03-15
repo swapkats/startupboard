db62ee215ac6061fc3452321150e8d594c04f3f1
Swapnil Singh <thisisnotaid@gmail.com>
2014-03-16 00:19:32 +0530

    Changes prod and dev database

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean-dev',
+    db: 'mongodb://localhost/startupBoard',
     app: {
         name: 'Startup Board'
     },
--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean',
+    db: 'mongodb://localhost/startupBoard',
     app: {
         name: 'MEAN - A Modern Stack - Production'
     },
