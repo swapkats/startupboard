20aa4cd2143c0eb5c50dc20e7dd6eca7961af74d
Swapnil Singh <thisisnotaid@gmail.com>
2014-03-16 18:06:45 +0530

    Fixes badcfg for query action

--- public/js/services/boards.js
+++ public/js/services/boards.js
@@ -2,5 +2,5 @@
 
 //Articles service used for articles REST endpoint
 angular.module('startupBoard').factory('Articles', ['$resource', function($resource) {
-    return $resource('board/:articleId', { articleId: '@_id' }, { update: { method: 'PUT'}});
+    return $resource('board/:articleId', { articleId: '@_id' }, { update: { method: 'PUT'}, query: {isArray:false}});
 }]);
